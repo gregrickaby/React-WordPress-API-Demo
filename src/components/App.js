fecac6c55999a63fd48e742ac4a896789439e9d8
Greg Rickaby <gregrickaby@gmail.com>
2018-03-22 07:12:30 -0500

    bring in fetch posts and set it to state

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,85 @@ import { Footer } from "./Footer";
 import "../css/styles.css";
 
 class App extends Component {
+  /**
+   * Setting context and default state.
+   *
+   * TO DO:
+   * - Create a "modal" component which will display the full blog post after clicking on the article title.
+   * - Control the modal via state (modalOpen = true/false)
+   */
+  constructor() {
+    super();
+    this.state = {
+      posts: []
+    };
+
+    // Set the API Url.
+    this.apiUrl = "https://gregrickaby.com/wp-json/wp/v2/posts";
+
+    // Set request args.
+    this.apiArgs = {
+      method: "GET",
+      mode: "cors",
+      cache: "default",
+      header: new Headers({
+        "Content-Type": "application/json"
+      })
+    };
+
+    // Set the request.
+    this.apiRequest = new Request(this.apiUrl, this.apiArgs);
+  }
+
+  /**
+   * Kick off a promise to fetch data from remote API.
+   */
+  fetchData() {
+    // Return the promise.
+    return (
+      fetch(this.apiRequest)
+        // If we have data...convert and return as JSON.
+        .then(response => response.json())
+
+        // Returning the results of the response.
+        .then(data => data)
+
+        // Catch any errors and bail!
+        .catch(error => {
+          console.error(error);
+        })
+    );
+  }
+
+  /**
+   * Since we're loading data from a remote endpoint,
+   * this is a good place to instantiate the network request.
+   */
+  componentDidMount() {
+    // Get the WP data.
+    const posts = this.fetchData();
+
+    // When the promise is resolved, set the data as state.
+    posts.then(post => {
+      this.setState({ posts: post });
+
+      // Toggle the body class.
+      document.body.classList.remove("not-ready");
+    });
+  }
+
   render() {
     return (
       <div className="app">
         <Header title="Hello World" />
-        <Article title="Post Title" content="Post content and stuff and all" />
+        {// Loop through our posts and return <Article> components.
+        this.state.posts.map(post => (
+          <Article
+            title={post.title.rendered}
+            excerpt={post.excerpt.rendered}
+            key={post.id}
+          />
+        ))}
         <Footer />
       </div>
     );
