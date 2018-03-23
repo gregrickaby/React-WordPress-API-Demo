624fb07caa5e2f88f0ca0cbb7e782a7fcdbf77cf
Greg Rickaby <gregrickaby@gmail.com>
2018-03-22 22:18:16 -0500

    wrap emoji in <span>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,11 @@ import React from "react";
 
 export const Footer = () => (
   <footer className="site-footer">
-    <p className="site-info">&copy; 2018 Greg Rickaby ☕</p>
+    <p className="site-info">
+      &copy; 2018 Greg Rickaby{" "}
+      <span role="img" aria-label="coffe">
+        ☕
+      </span>
+    </p>
   </footer>
 );
