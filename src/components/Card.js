e440c8a670f563068b6cbd6868cac239f3d95b09
Greg Rickaby <gregrickaby@gmail.com>
2018-03-27 14:09:46 -0500

    remove Router for now

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,20 @@
 import React, { Component } from "react";
-import { Router, Link } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
 import { createDangerousMarkup } from "../helpers";
 
 class Card extends Component {
   render() {
     return (
-      <Router>
-        <article className="card">
-          <h1
-            className="post-title"
-            dangerouslySetInnerHTML={createDangerousMarkup(this.props.title)}
-          />
-          <div
-            className="post-excerpt"
-            dangerouslySetInnerHTML={createDangerousMarkup(this.props.content)}
-          />
-          <Link to="" />
-        </article>
-      </Router>
+      <article className="card">
+        <h1
+          className="post-title"
+          dangerouslySetInnerHTML={createDangerousMarkup(this.props.title)}
+        />
+        <div
+          className="post-excerpt"
+          dangerouslySetInnerHTML={createDangerousMarkup(this.props.content)}
+        />
+      </article>
     );
   }
 }
