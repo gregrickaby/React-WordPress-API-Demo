a2505ce24e6912ba26ab856662fc36fbeae79c4e
Greg Rickaby <gregrickaby@gmail.com>
2018-03-27 21:56:25 -0500

    use <Router />

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import Router from "./components/Router";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Router />, document.getElementById("root"));
 registerServiceWorker();
