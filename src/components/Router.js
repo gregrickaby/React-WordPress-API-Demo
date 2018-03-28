5bc9c3999af16503f2675899b85f5b3b167ea7bf
Greg Rickaby <gregrickaby@gmail.com>
2018-03-28 10:42:26 -0500

    set up single post routing with id/slug

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from "../pages/Home";
+import App from "../components/App";
 import Single from "../pages/Single";
 import NotFound from "../pages/NotFound";
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/blog/:id" component={Single} />
+      <Route exact path="/" component={App} />
+      <Route path="/single/:id/:slug/" component={Single} />
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
