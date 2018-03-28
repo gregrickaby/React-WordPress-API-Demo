b7ed3c98c391d0fd56a184327b943842d4f1412a
Greg Rickaby <gregrickaby@gmail.com>
2018-03-28 11:26:20 -0500

    add new route to /about

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import App from "../components/App";
+import About from "../pages/About";
 import Single from "../pages/Single";
 import NotFound from "../pages/NotFound";
 
@@ -8,6 +9,7 @@ const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={App} />
+      <Route exact path="/about" component={About} />
       <Route path="/single/:id/:slug/" component={Single} />
       <Route component={NotFound} />
     </Switch>
