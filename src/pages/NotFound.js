d3d344c46e30d113a2e560f85ee4c7ac84ed220a
Greg Rickaby <gregrickaby@gmail.com>
2018-03-28 12:06:32 -0500

    better 404 page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Header } from "../components/Header";
+import { Menu } from "../components/Menu";
+import { Footer } from "../components/Footer";
 
-const NotFound = () => (
-  <div>
-    <h2>
-      404 Not Found!!{" "}
-      <span role="img" aria-label="Wow">
-        😳
-      </span>
-    </h2>
-  </div>
-);
+class NotFound extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Header title="My latest blog posts" />
+        <Menu />
+        <section className="single-container not-found">
+          <h1>
+            404 Not Found!!{" "}
+            <span role="img" aria-label="Wow">
+              😳
+            </span>
+          </h1>
+          <p>Sorry about that. It looks like there's nothing here.</p>
+          <p>
+            <Link to="/">Return to the homepage</Link>
+          </p>
+        </section>
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
 
 export default NotFound;
