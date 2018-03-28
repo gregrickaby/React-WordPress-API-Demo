6424319ef18d50112dd1348ef40cd1ca30e2e06b
Greg Rickaby <gregrickaby@gmail.com>
2018-03-28 10:43:48 -0500

    grab title/content from props

--- src/pages/Single.js
+++ src/pages/Single.js
@@ -3,15 +3,16 @@ import { createDangerousMarkup } from "../helpers";
 
 class Single extends Component {
   render() {
+    const postData = this.props.location.state;
     return (
       <article className="post">
         <h1
           className="post-title"
-          dangerouslySetInnerHTML={createDangerousMarkup(this.props.title)}
+          dangerouslySetInnerHTML={createDangerousMarkup(postData.title)}
         />
         <div
           className="post-excerpt"
-          dangerouslySetInnerHTML={createDangerousMarkup(this.props.content)}
+          dangerouslySetInnerHTML={createDangerousMarkup(postData.content)}
         />
       </article>
     );
