bfcc082dc865a6a778af0945019e89261307226d
Greg Rickaby <gregrickaby@gmail.com>
2018-03-28 11:26:45 -0500

    add other components

--- src/pages/Single.js
+++ src/pages/Single.js
@@ -1,20 +1,30 @@
 import React, { Component } from "react";
 import { createDangerousMarkup } from "../helpers";
+import { Header } from "../components/Header";
+import { Menu } from "../components/Menu";
+import { Footer } from "../components/Footer";
 
 class Single extends Component {
   render() {
     const postData = this.props.location.state;
     return (
-      <article className="post">
-        <h1
-          className="post-title"
-          dangerouslySetInnerHTML={createDangerousMarkup(postData.title)}
-        />
-        <div
-          className="post-excerpt"
-          dangerouslySetInnerHTML={createDangerousMarkup(postData.content)}
-        />
-      </article>
+      <React.Fragment>
+        <Header title="My latest blog posts" />
+        <Menu />
+        <section className="single-container">
+          <article className="post">
+            <h1
+              className="post-title"
+              dangerouslySetInnerHTML={createDangerousMarkup(postData.title)}
+            />
+            <div
+              className="post-excerpt"
+              dangerouslySetInnerHTML={createDangerousMarkup(postData.content)}
+            />
+          </article>
+        </section>
+        <Footer />
+      </React.Fragment>
     );
   }
 }
