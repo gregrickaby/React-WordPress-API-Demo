477774b3392e7413e4f95e3adc29a323b1c9fd1d
Greg Rickaby <gregrickaby@gmail.com>
2019-06-03 10:09:19 -0500

    lowercase

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import IndexPage from "../pages/index";
-import About from "../pages/About";
-import Single from "../pages/Single";
-import NotFound from "../pages/NotFound";
+import About from "../pages/about";
+import Single from "../pages/single";
+import NotFound from "../pages/not-found";
 
 const Router = () => (
   <BrowserRouter>
