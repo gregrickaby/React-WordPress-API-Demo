17cadd23195e74952db8e5565f79916d3a034224
Greg Rickaby <gregrickaby@gmail.com>
2019-06-03 15:01:53 -0500

    add react-snap for static prerendering

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { hydrate, render } from "react-dom";
 import Router from "./router";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<Router />, document.getElementById("page"));
-
+const rootElement = document.getElementById("page");
+if (rootElement.hasChildNodes()) {
+  hydrate(<Router />, rootElement);
+} else {
+  render(<Router />, rootElement);
+}
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
