7f250be168bb6b0d59841920510c03a7361197dd
Greg Rickaby <gregrickaby@gmail.com>
2019-06-03 12:31:54 -0500

    don't use main

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import Router from "./router";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<Router />, document.getElementById("root"));
+ReactDOM.render(<Router />, document.getElementById("page"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
