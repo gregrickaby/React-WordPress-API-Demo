3bc78eacfda66e0e4a3afb57a6a0e4b5c9a89ce6
Greg Rickaby <gregrickaby@gmail.com>
2019-06-03 12:48:26 -0500

    use createGlobalStyle

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -2,6 +2,5 @@ import styled from "styled-components";
 
 export const FooterLayout = styled.footer`
   font-size: 14px;
-  margin: 24px auto;
   text-align: center;
 `;
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const HeaderLayout = styled.header`
-  margin: 24px auto;
   text-align: center;
 
   .app-logo {
