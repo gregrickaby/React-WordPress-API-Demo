e14f6c192b9e347d5b5a74b7240cea0d596b7824
Greg Rickaby <gregrickaby@gmail.com>
2019-06-03 12:34:45 -0500

    remove /css

--- src/components/Layout/styles.js
+++ src/components/Layout/styles.js
@@ -5,12 +5,56 @@ export const SiteLayout = styled.div`
   color: #231f20;
   font-family: sans-serif;
 
+  body {
+    background-color: #f7f7f7;
+    color: #231f20;
+    font-family: sans-serif;
+  }
+
   a {
     color: #05a2d3;
 
     &:visited,
     &:active {
       color: #05a2d3;
+    }
+  }
+
+  .left {
+    float: left;
+    margin-right: 24px;
+  }
+
+  .right {
+    float: right;
+    margin-left: 24px;
+  }
+
+  img {
+    height: auto;
+    max-width: 100%;
+  }
+
+  embed,
+  iframe,
+  object {
+    max-width: 100%;
+  }
+
+  .fluid-embed {
+    height: 0;
+    overflow: hidden;
+    padding-bottom: 56.25%; // For ratio 16:9. 75% if ratio is 4:3
+    position: relative;
+
+    embed,
+    object,
+    iframe {
+      position: absolute;
+      top: 0;
+      left: 0;
+      height: 100%;
+      width: 100%;
     }
   }
 `;
