70f24d89598d05af08dde56672d8b5679a8e9d37
Greg Rickaby <greg@gregrickaby.com>
2020-02-29 09:15:42 -0600

    use Homepage

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import IndexPage from "./templates/index";
+import Homepage from "./templates/homepage";
 import Pages from "./templates/pages";
 import Posts from "./templates/posts";
 import NotFound from "./templates/not-found";
@@ -8,7 +8,7 @@ import NotFound from "./templates/not-found";
 const Router = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={IndexPage} />
+      <Route exact path="/" component={Homepage} />
       <Route exact path="/:slug" component={Pages} />
       <Route path="/blog/:id/:slug/" component={Posts} />
       <Route component={NotFound} />
