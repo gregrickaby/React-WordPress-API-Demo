7173f06ae3f1be7b8886a5e08b812535cb2b697f
Greg Rickaby <greg@gregrickaby.com>
2020-02-29 09:10:08 -0600

    about is now pages

---
+++ src/templates/pages.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Layout from "../components/Layout";
+
+class Pages extends Component {
+  render() {
+    return (
+      <>
+        <Layout>
+          <article className="post">Hello from the Pages template.</article>
+        </Layout>
+      </>
+    );
+  }
+}
+
+export default Pages;
