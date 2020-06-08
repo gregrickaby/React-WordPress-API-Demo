cf9ec32f94f1577db82c78093b66227a459816be
Greg Rickaby <greg@gregrickaby.com>
2020-06-08 09:36:24 -0500

    update siteURL

--- src/config.js
+++ src/config.js
@@ -3,9 +3,9 @@ export const config = {
     title: `React WordPress REST API Demo`,
     description: `Create a single page web app with ReactJS using WordPress as a headless CMS.`,
     author: `@gregrickaby`,
-    siteUrl: `https://react-wordpress-api-demo.netlify.com/`
+    siteUrl: `https://react-wordpress-api-demo.netlify.app/`,
   },
   options: {
-    wordPressUrl: "https://webdevstudios.com/wp-json/wp/v2/posts"
-  }
+    wordPressUrl: "https://webdevstudios.com/wp-json/wp/v2/posts",
+  },
 };
